4c9a89942006cc9803f5592e165f7d00ddf03000
DimZZyJ <jeltov.dima@yandex.ru>
2022-10-21 13:56:45 +0300

    some file gelpers methods

--- FileHelper.cs
+++ FileHelper.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,25 @@ namespace Lib_Sbis
 {
     public class FileHelper
     {
+        public FileInfo[] GetFilesInfoList()
+        {
+            string Dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\zip";
+            DirectoryInfo directoryInfo = new DirectoryInfo(Dir);
+            FileInfo[] files = directoryInfo.GetFiles("*.zip");
+            string tmp = Dir + @"\Счет-фактура полученный № 7775 от 16.09.2022 на сумму 67100.00.zip";
+            //GetXMLFromZip(tmp);
+            readZipFile(tmp);
+            return files;
+        }
+        public void GetXMLFromZip(string path)
+        {
+            using (var zip = ZipFile.Open(path,ZipArchiveMode.Read))
+            {
+                var smt = zip.Entries;
+            }
+        }
         //TODO: организуй парсинг xml из архива
         //TODO: Парсин из массива 
-        
+
     }
 }
